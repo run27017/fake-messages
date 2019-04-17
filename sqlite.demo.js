2a174a75e9b799c2fcf2f39adc145602fbf1bf4e
run27017 <run27017@126.com>
2019-04-17 17:22:11 +0800

    引入sqlite3

--- sqlite.demo.js
+++ sqlite.demo.js
@@ -1,9 +1,7 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('db/dev.sqlite3');
 
 db.serialize(function() {
-
-  db.run('CREATE TABLE lorem (info TEXT)');
   var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
 
   for (var i = 0; i < 10; i++) {
