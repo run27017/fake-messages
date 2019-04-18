5d157c671369ebe3700d23f9aff3790ecae614a4
run27017 <run27017@126.com>
2019-04-18 11:56:13 +0800

    增加创建邮件的接口

--- routes/emails.js
+++ routes/emails.js
@@ -7,7 +7,7 @@ function getEmails (callback) {
 
   db.all("SELECT * FROM email", function(err, rows) {
     if (err) {
-      callback(err);
+      callback && callback(err);
     } else {
       callback && callback(null, rows);
     }
@@ -16,14 +16,38 @@ function getEmails (callback) {
   db.close();
 }
 
-/* GET emails listing. */
+function createEmail (email, callback) {
+  var db = new sqlite3.Database('db/dev.sqlite3');
+
+  db.run('INSERT INTO email(content, createdAt) VALUES (?, ?)', [email.content, email.createdAt], function (err) {
+    if (err) {
+      callback && callback(err);
+    } else {
+      callback && callback(null);
+    }
+  });
+
+  db.close();
+}
+
 router.get('/', function(req, res, next) {
   getEmails(function (err, emails) {
     if (err) {
-      console.error('从数据库中好获取邮箱列表失败', err)
+      console.error('从数据库中获取邮箱列表失败', err)
       res.status(500).send({ error: err });
     } else {
       res.send(emails);
+    }
+  });
+});
+
+router.post('/', function(req, res, next) {
+  createEmail(req.body, function (err) {
+    if (err) {
+      console.error('在数据库中创建邮箱失败', err)
+      res.status(500).send({ error: err });
+    } else {
+      res.status(201).send('created');
     }
   });
 });
