3829a9d2cca71cec95f8056bc0e0d2bd6b819e96
run27017 <run27017@126.com>
2019-04-19 09:56:23 +0800

    更好地显示时间

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,18 @@ new Vue({
   components: {
     'i-table': Table
   },
+  filters: {
+    datetime (timestamps) {
+      const date = new Date(timestamps)
+      const year = date.getFullYear()
+      const month = date.getMonth()
+      const day = date.getDate()
+      const hours = date.getHours()
+      const minutes = date.getMinutes()
+      const seconds = date.getSeconds()
+      return `${year}年${month}月${day}日 ${hours}:${minutes}:${seconds}`
+    }
+  },
   data: {
     columns: [
       {
